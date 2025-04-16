a5837e9fbcf0074ae3831b235a12ece083da2f4a
ticaki <ticaki@renopoint.de>
2025-04-16 15:22:52 +0200

    split tasmota init

--- src/main.ts
+++ src/main.ts
@@ -793,8 +793,7 @@ class NspanelLovelaceUi extends utils.Adapter {
                                     ` MqttPort ${obj.message.mqttPort}; MqttUser ${obj.message.mqttUsername}; MqttPassword ${obj.message.mqttPassword};` +
                                     ` FullTopic ${`${topic}/%prefix%/`.replaceAll('//', '/')};` +
                                     ` MqttRetry 10; FriendlyName1 ${obj.message.tasmotaName}; Hostname ${obj.message.tasmotaName.replaceAll(/[^a-zA-Z0-9_-]/g, '_')};` +
-                                    ` WebLog 2; template {"NAME":"${obj.message.tasmotaName}", "GPIO":[0,0,0,0,3872,0,0,0,0,0,32,0,0,0,0,225,0,480,224,1,0,0,0,33,0,0,0,0,0,0,0,0,0,0,4736,0],"FLAG":0,"BASE":1};` +
-                                    ` Module 0; MqttClient ${mqttClientId};${this.config.timezone ? definition.getTasmotaTimeZone(this.config.timezone) : ''}` +
+                                    ` MqttClient ${mqttClientId};` +
                                     ` ${obj.message.mqttServer ? 'SetOption132 1; SetOption103 1 ' : 'SetOption132 0; SetOption103 0'}; Restart 1`;
                                 u = new URL(
                                     `http://${obj.message.tasmotaIP}/cm?` +
@@ -803,6 +802,17 @@ class NspanelLovelaceUi extends utils.Adapter {
                                 );
                                 this.log.info(
                                     `Sending mqtt config & base config to tasmota with IP ${obj.message.tasmotaIP} and name ${obj.message.tasmotaName}.`,
+                                );
+                                await axios.get(u.href);
+                                this.mqttClient && (await this.mqttClient.waitPanelConnectAsync(topic, 60000));
+
+                                u = new URL(
+                                    `http://${obj.message.tasmotaIP}/cm?` +
+                                        `${this.config.useTasmotaAdmin ? `user=admin&password=${this.config.tasmotaAdminPassword}` : ``}` +
+                                        `&cmnd=Backlog${encodeURIComponent(
+                                            ` WebLog 2; template {"NAME":"${obj.message.tasmotaName}", "GPIO":[0,0,0,0,3872,0,0,0,0,0,32,0,0,0,0,225,0,480,224,1,0,0,0,33,0,0,0,0,0,0,0,0,0,0,4736,0],"FLAG":0,"BASE":1};` +
+                                                ` Module 0;${this.config.timezone ? definition.getTasmotaTimeZone(this.config.timezone) : ''}: restart 1`,
+                                        )}`,
                                 );
 
                                 await axios.get(u.href);
