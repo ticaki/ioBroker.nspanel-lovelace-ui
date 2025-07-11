f66fcdf249b5fa9f8173a91a9548e37b67df33b0
ticaki <59184294+ticaki@users.noreply.github.com>
2025-07-11 22:58:48 +0200

    translations (#346)
    Co-authored-by: ticaki <ticaki@renopoint.de>

--- src/lib/classes/config-manager.ts
+++ src/lib/classes/config-manager.ts
@@ -1358,13 +1358,7 @@ export class ConfigManager extends BaseClass {
                             false: { type: 'const', constVal: 'off' },
                         },
                         text: text,
-                        entity1:
-                            role == 'ct' || role == 'hue' || role === 'rgb' || role === 'rgbSingle'
-                                ? { value: foundedStates[role].ON_ACTUAL }
-                                : role === 'dimmer'
-                                  ? { value: foundedStates[role].ON_ACTUAL }
-                                  : { value: foundedStates[role].ON_ACTUAL },
-
+                        entity1: { value: foundedStates[role].ON_ACTUAL },
                         setNavi: item.targetPage ? await this.getFieldAsDataItemConfig(item.targetPage) : undefined,
                     },
                 };
