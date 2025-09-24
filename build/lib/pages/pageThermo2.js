2ddf763566ba39ecd3270a1981a5f2bbb84b2166
ticaki <ticaki@renopoint.de>
2025-09-24 10:25:08 +0200

    more log

--- build/lib/pages/pageThermo2.js
+++ build/lib/pages/pageThermo2.js
@@ -533,6 +533,7 @@ class PageThermo2 extends import_pageMenu.PageMenu {
           continue;
         }
       }
+      configManager.log.error(`Check set ${set} ${await configManager.existsState(set)} ${set.endsWith(".")}`);
       if (!await configManager.existsAndWriteableState(set)) {
         set = "";
         const msg = `${page.uniqueName} item: ${i} id: ${set} invalid SET datapoint. Not exists or not writeable!`;
