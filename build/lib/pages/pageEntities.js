518290ff04415e6c823d6284efbf29efc82e6825
ticaki <ticaki@renopoint.de>
2025-10-20 18:31:33 +0200

    reduce to 500ms

--- build/lib/pages/pageEntities.js
+++ build/lib/pages/pageEntities.js
@@ -44,7 +44,7 @@ class PageEntities extends import_pageMenu.PageMenu {
     if (options.items && options.items.card == "cardEntities") {
       this.items = options.items;
     }
-    this.minUpdateInterval = 800;
+    this.minUpdateInterval = 500;
   }
   async init() {
     const config = structuredClone(this.config);
