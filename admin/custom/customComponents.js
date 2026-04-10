a737bb35416a87fb203ac2053e8bcfeb97c61fc9
tt-tom17 <tgb@kabelmail.de>
2026-04-10 22:22:33 +0200

    separate componente Colorsetting Dialog

--- admin/custom/customComponents.js
+++ admin/custom/customComponents.js
@@ -1,1 +1,1 @@
-import"./assets/preload-helper-BDBacUwf.js";import{g as r,i as a}from"./assets/virtual_mf-REMOTE_ENTRY_ID_AdminComponentEasyAccessSet__customComponents_js-D6Y4ScC6.js";export{r as get,a as init};
+import"./assets/preload-helper-BDBacUwf.js";import{g as r,i as a}from"./assets/virtual_mf-REMOTE_ENTRY_ID_AdminComponentEasyAccessSet__customComponents_js-DDOeJ_6i.js";export{r as get,a as init};
