aa21b3e2de7cb507ad99bde4b798c7fdbd83e7f2
ticaki <ticaki@renopoint.de>
2026-04-14 14:04:57 +0200

    update build

--- admin/custom/customComponents.js
+++ admin/custom/customComponents.js
@@ -1,1 +1,1 @@
-import"./assets/preload-helper-BDBacUwf.js";import{g as r,i as a}from"./assets/virtual_mf-REMOTE_ENTRY_ID_AdminComponentEasyAccessSet__customComponents_js-CXBXkB8X.js";export{r as get,a as init};
+import"./assets/preload-helper-BDBacUwf.js";import{g as r,i as a}from"./assets/virtual_mf-REMOTE_ENTRY_ID_AdminComponentEasyAccessSet__customComponents_js--KeuR6qH.js";export{r as get,a as init};
