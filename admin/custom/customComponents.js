2287804d96e8542242bc05e90f29f3529123dc0f
tt-tom17 <tgb@kabelmail.de>
2026-05-12 18:42:27 +0200

    fix translate

--- admin/custom/customComponents.js
+++ admin/custom/customComponents.js
@@ -1,1 +1,1 @@
-import"./assets/preload-helper-BDBacUwf.js";import{g as r,i as a}from"./assets/virtual_mf-REMOTE_ENTRY_ID_AdminComponentEasyAccessSet__customComponents_js-B19Vl3SN.js";export{r as get,a as init};
+import"./assets/preload-helper-BDBacUwf.js";import{g as r,i as a}from"./assets/virtual_mf-REMOTE_ENTRY_ID_AdminComponentEasyAccessSet__customComponents_js-Cz7dwNwu.js";export{r as get,a as init};
